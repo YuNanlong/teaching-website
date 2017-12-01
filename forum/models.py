8f376f44e2bc6a4e828f472c21119fc35f400012
Mellolo <876684522@qq.com>
2017-12-01 22:39:22 +0800

    forum version2

--- forum/models.py
+++ forum/models.py
@@ -2,6 +2,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from teach.models import Course
+from teach.storage import FileStorage
 
 # Create your models here.
 
@@ -12,18 +13,18 @@ class Section(models.Model):
 class Post(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     poster = models.ForeignKey(User, on_delete=models.CASCADE)
-    section = models.ForeignKey(Section, null= True,on_delete=models.CASCADE)
-    title = models.CharField(max_length=25)
-    content = models.CharField(max_length=1000, default='')
+    section = models.ForeignKey(Section, null= True,on_delete=models.SET_NULL)
+    title = models.CharField(max_length=100)
+    content = models.CharField(max_length=4000, default='')
     is_top = models.IntegerField(default=0)
     is_best = models.IntegerField(default=0)
     time = models.DateTimeField(auto_now_add=True)
     layer_number = models.IntegerField(default=0)
-    files = models.FileField(upload_to='files/')
+    files = models.FileField(upload_to='files/', storage=FileStorage())
 
 
 class Reply(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     replier = models.ForeignKey(User, on_delete=models.CASCADE)
-    content = models.CharField(max_length=256, default='')
+    content = models.CharField(max_length=1024, default='')
     time = models.DateTimeField(auto_now_add=True)
--- forum/views.py
+++ forum/views.py
@@ -2,15 +2,13 @@
 from __future__ import unicode_literals
 from django.shortcuts import render,redirect
 from .models import *
-from django.contrib.auth.models import User
-from django.contrib.auth import authenticate
-from django.contrib import auth
 from django.contrib.auth.decorators import login_required
 from django.http import Http404,StreamingHttpResponse
 from teach.models import Course
+from django.db.models import Q
+from django.core.paginator import Paginator
 from account.views import home
 from django import template
-from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 
 # Create your views here.
 
@@ -36,43 +34,47 @@ def homepage(request, id, status = "true"):
         course = Course.objects.get(id = course_id)
     except ValueError:
         raise  Http404()
+
     course_name = course.name
     sections = Section.objects.all().order_by("id")
     section_list = []
+
+    # 默认板块帖子
+    default_section = {}
+    default_section['section_id'] = "null"
+    default_section['section_name'] = default_section_name
+    default_post_list = []
+    default_section_posts = Post.objects.filter(course=course, section=None).order_by("-is_top", "-time")
+    for post in default_section_posts:
+        default_post_list.append(
+            {'id': post.id, 'title': post.title, 'poster': post.poster.username, 'is_best': post.is_best,
+             'time': post.time})
+    default_section['post_list'] = default_post_list
+    section_list.append(default_section)
+
     for section in sections:
         tmp_section = {}
         tmp_section['section_id'] = section.id
         tmp_section['section_name'] = section.name
         tmp_post_list = []
-        course_section_posts = Post.objects.filter(course=course,section = section).order_by("-is_top","-time")
+        course_section_posts = Post.objects.filter(course=course, section=section).order_by("-is_top", "-time")
         for post in course_section_posts:
             if post.is_best == 0:
                 is_best = '普通帖'
             else:
-                is_best = '精华帖';
-            tmp_post_list.append({'id':post.id,'title':post.title,'poster':post.poster.username,'is_best':is_best,'time':post.time})
+                is_best = '精华帖'
+            tmp_post_list.append(
+                {'id': post.id, 'title': post.title, 'poster': post.poster.username, 'is_best': is_best,
+                 'time': post.time})
         tmp_section['post_list'] = tmp_post_list
         section_list.append(tmp_section)
-    #默认板块帖子
-    default_section = {}
-    default_section['section_id'] = "null"
-    default_section['section_name'] = default_section_name
-    default_post_list = []
-    default_section_posts = Post.objects.filter(course=course,section = models.SET_NULL).order_by("-is_top","-time")
-    for post in default_section_posts:
-        if post.is_best == 0:
-            is_best = '普通帖'
-        else:
-            is_best = '精华帖';
-        default_post_list.append(
-            {'id': post.id, 'title': post.title, 'poster': post.poster.username, 'is_best': is_best, 'time': post.time})
-    default_section['post_list'] = default_post_list
-    section_list.append(default_section)
 
-    instance = {'user':user,'couse_name':course_name,'course_id':course_id,'section_list':section_list,'status':status}
+    instance = {'user': user, 'course_name': course_name, 'course_id': course_id, 'section_list': section_list,
+                'status': status}
+
     if request.method == 'POST':
-        return render(request, 'homepage.html',instance)
-    else:
+        return render(request, 'homepage.html', instance)
+    else:#GET method
         return render(request, 'homepage.html',instance)
 
 @login_required
@@ -89,12 +91,12 @@ def release_post(request, id):
         title_length = len(request.POST['post_title'])
         content_length = len(request.POST['post_content'])
         error = False
-        if title_length <=0 or title_length > 25:
+        if title_length <=0 or title_length > 100:
             error = True
-        elif content_length <=0 or content_length > 1000:
+        elif content_length <=0 or content_length > 4000:
             error = True
         if error:
-            return
+            raise Http404()
         else:
             ptitle = request.POST.get('post_title')
             pcontent = request.POST.get('post_content')
@@ -104,9 +106,9 @@ def release_post(request, id):
                 psectionid = int(psectionid)
             except ValueError:
                 flag = 1
-                psection = models.SET_NULL()
+                psection = None
             if flag == 0:
-                psection = Section.object.get(id=psectionid)
+                psection = Section.objects.get(id=psectionid)
             pcourse = Course.objects.get(id = id)
             post = Post.objects.create(
                 poster=request.user,
@@ -118,7 +120,6 @@ def release_post(request, id):
             if 'post_file' in request.FILES:
                 pfile = request.FILES.get('post_file')
                 post.files = pfile
-                post.files.name = str(post.id) + '__' + str(pfile.name)
             post.save()
             return homepage(request, course_id, "发帖成功")
     else:
@@ -147,7 +148,14 @@ def watch_post(request,id, status = "true"):
     replies = []
     for r in the_replies:
         replies.append({'id': r.id, 'replier': r.replier.username, 'replier_id': r.replier.id, 'content': r.content,'time': r.time})
-    instance = {'user':request.user,'course_id':course_id,'course_name':course_name,'post':post,'replies':replies,'no_best':no_best,'no_top':no_top,'status':status}
+    if the_post.files != '':
+        filename = the_post.files.name
+        filename = filename.split('/')[1]
+        exist_file = 1
+    else:
+        filename = ''
+        exist_file = 0
+    instance = {'user':request.user,'exist_file':exist_file,'filename':filename,'course_id':course_id,'course_name':course_name,'post':post,'replies':replies,'no_best':no_best,'no_top':no_top,'status':status}
     if request.method == 'POST':
         return render(request, 'watch_post.html',instance)
     else:
@@ -166,10 +174,10 @@ def release_reply(request, id):
     if request.method == 'POST':
         content_length = len(request.POST.get('reply_content'))
         error = False
-        if content_length <=0 or content_length > 256:
+        if content_length <=0 or content_length > 1024:
             error = True
         if error:
-            return
+            raise Http404()
         else:
             rcontent = request.POST.get('reply_content')
             reply = Reply.objects.create(
@@ -269,7 +277,7 @@ def delete_post(request,id):
         return homepage(request,course_id,"未能删除帖子")
 
 @login_required
-def add_section(request):
+def add_section(request,id):
     try:
         course_id = int(id)
     except ValueError:
@@ -282,10 +290,8 @@ def add_section(request):
         if getUserType(request.user) != 'teacher':
             return redirect("home")
         addsection_name = request.POST.get('add_section_name')
-        if addsection_name == default_section_name:
-            return
-        if Section.objects.filter(course = find_the_course, name = addsection_name):
-            return
+        if addsection_name == default_section_name or Section.objects.filter(course = find_the_course, name = addsection_name):
+            return homepage(request, course_id, "未能添加板块,请选取其他版块名")
         addtosection = Section.objects.create(
             name = addsection_name,
             course = find_the_course,
@@ -308,16 +314,48 @@ def delete_section(request, id):
     if request.method == 'POST':
         if getUserType(request.user) != 'teacher':
             return redirect("home")
-        delsection_id = request.POST.get('delete_section_id')
+        if 'delete_section_id' in request.POST:
+            delsection_id = request.POST.get('delete_section_id')
+        else:
+            return homepage(request,course_id,"未能删除不存在的板块")
         try:
             delsection_id = int(delsection_id)
         except:
-            return
+            raise Http404()
         delsection = Section.objects.get(id = delsection_id)
         delsection.delete()
         return homepage(request,course_id,"成功删除板块")
     else:
         return homepage(request,course_id,"未能删除板块")
+
+@login_required
+def search(request,id):
+    try:
+        course_id = int(id)
+    except ValueError:
+        raise Http404()
+    try:
+        find_the_course = Course.objects.get(id=course_id)
+    except ValueError:
+        raise Http404()
+    if request.method == 'POST':
+        if 'search_content' in request.POST:
+            key = request.POST.get('search_content')
+        else:
+            return homepage(request,course_id,"未能搜索不存在的内容")
+        post_filter = Post.objects.filter(Q(course = find_the_course),Q(title__contains = key) | Q(content__contains = key)).order_by("-is_top", "-time")
+        post_paging = Paginator(post_filter,5)
+        page_list = []
+        for page_num in post_paging.page_range:
+            tmp_page = {}
+            tmp_page['page_id'] = page_num
+            tmp_page['post_list'] = post_paging.page(page_num)
+            page_list.append(tmp_page)
+        instance = {'page_list':page_list,'course_id':course_id,'course_name':find_the_course.name,'user':request.user}
+        return render(request,'search.html',instance)
+    else:
+        return redirect("home")
+
 
 
 @login_required
@@ -329,8 +367,8 @@ def download(request,id):
     post = Post.objects.get(id=offset)
     filename = post.files.name
 
-    def file_iterator(file_name, chunk_size=512):
-        with open(file_name) as f:
+    def file_iterator(file_name, chunk_size=10000):
+        with open(file_name, 'rb') as f:
             while True:
                 c = f.read(chunk_size)
                 if c:
@@ -338,7 +376,7 @@ def download(request,id):
                 else:
                     break
     the_file_name = post.files.name
-    response = StreamingHttpResponse(file_iterator(filename))
+    response = StreamingHttpResponse(file_iterator('media/'+filename))
     response['Content-Type'] = 'application/octet-stream'
     response['Content-Disposition'] = 'attachment;filename="{0}"'.format(the_file_name)
     return response
